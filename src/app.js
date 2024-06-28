a9fce42e917262443a813530adbd8882b28bbd02
GDuranHenriquez <gregorioduranedrian123@gmail.com>
2024-06-28 15:07:29 -0400

    GDuran fix rturn in resp

--- src/app.js
+++ src/app.js
@@ -22,7 +22,12 @@ const routerDepartamento = require('./routes/routerDepartamento.js')
 require('./db.js');
 
 const server = express();
-server.use(cors());
+server.use(cors({
+  origin: '*',
+  credentials: true,
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  methods: 'GET, POST, OPTIONS, PUT, DELETE',
+}));
 
 server.name = 'Magnament Inventari';
 
@@ -30,13 +35,13 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-/* server.use((req, res, next) => {
+server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
-}); */
+});
 
 //server.use('/', routes);
 server.use('/data-user', authenticate, routerDataUser);
