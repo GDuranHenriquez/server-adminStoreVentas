a8a6f812809b2e1fc293e62fc8eee14f0fab3d4d
GDuranHenriquez <gregorioduranedrian123@gmail.com>
2024-07-16 01:51:17 -0400

    GD fix reporte

--- src/controllers/ventas/getCutClose.js
+++ src/controllers/ventas/getCutClose.js
@@ -127,7 +127,7 @@ async function getCutClose(req, res){
           const arrVentaDetalle = listDetalleVenta.filter((detalle) => detalle.idVenta === venta.id);
           DataVentasDetalles.push(arrVentaDetalle)
         }
-        
+        console.log(DataVentasDetalles, initDay, endDay, depart.nombre, infoCuotClose, recividoEfectivo)
         const htmlAllData = htmlCutClose(DataVentasDetalles, initDay, endDay, depart.nombre, infoCuotClose, recividoEfectivo)
         const bs64Pdf = await generatePdfCutClose(htmlAllData);
                 
